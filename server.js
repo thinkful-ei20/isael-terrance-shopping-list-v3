de25ddaba83dff508a7a6c88672d12300725beea
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2018-04-18 11:17:21 -0600

    created delete endpoint for recipes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -75,7 +75,15 @@ app.post('/recipes', jsonParser, (req, res) => {
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
-})
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  let {id} = req.params;
+  Recipes.delete(id);
+  console.log(`deleting ${id} item`);
+  res.status(204);
+  res.end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
